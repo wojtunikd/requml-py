3445f0da5ac2b1f8fe32362c00740bc6e0f0b171
Damian <wojtunikdw@gmail.com>
2021-08-06 18:40:02 +0100

    Added new API routes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_restful import Api, Resource
 
 from bson.json_util import dumps
 
-from controller import getOrder, updateActorUseCases, getOrderUseCases, getAllOrders
+from controller import getOrder, updateActorUseCases, getOrderUseCases, getAllOrders, deleteAllOrders, deleteAnOrder
 from Analysis.use_cases import analyseForUseCases
 
 load_dotenv()
@@ -24,9 +24,23 @@ def verifyOrder(order):
         return {"message": "Order initiated", "ucParam": ucUpdate["useCasesParam"]}, 200
 
 
+def rerunAnalysis(order):
+    if order is None:
+        return {"message": "The order has not been found"}, 404
+    else:
+        actorsWithUseCases = analyseForUseCases(order)
+        ucUpdate = updateActorUseCases(order["_id"], actorsWithUseCases)
+        return {"message": "Analysis has been rerun", "ucParam": ucUpdate["useCasesParam"]}, 200
+
+
 class InitiateAnalysis(Resource):
     def post(self, orderId):
         return verifyOrder(getOrder(orderId))
+
+
+class RerunAnalysis(Resource):
+    def post(self, orderId):
+        return rerunAnalysis(getOrder(orderId))
 
 
 class OrderUseCases(Resource):
@@ -43,6 +57,9 @@ class OrderDetails(Resource):
     def get(self, orderId):
         order = getOrder(orderId)
 
+        if order is None:
+            return {"message": "The order has not been found."}, 404
+
         return Response(
             dumps(order, indent=2),
             mimetype='application/json'
@@ -58,8 +75,26 @@ class AllOrders(Resource):
         )
 
 
+class DeleteOrder(Resource):
+    def delete(self, orderId):
+        deleted = deleteAnOrder(orderId)
+        if deleted is None:
+            return {"message": "The order couldn't be found and deleted."}, 404
+        else:
+            return {"message": "The order has been deleted successfully."}, 200
+
+
+class DeleteAllOrders(Resource):
+    def delete(self):
+        deleteAllOrders()
+        return {"message": "All the existing orders have been deleted successfully."}, 200
+
+
 api.add_resource(InitiateAnalysis, "/initiate/<string:orderId>")
 api.add_resource(OrderUseCases, "/uc/<string:orderId>")
+api.add_resource(RerunAnalysis, "/orders/rerun/<string:orderId>")
+api.add_resource(DeleteOrder, "/orders/delete/<string:orderId>")
+api.add_resource(DeleteAllOrders, "/orders/delete/all")
 api.add_resource(OrderDetails, "/orders/<string:orderId>")
 api.add_resource(AllOrders, "/orders/all")
 
