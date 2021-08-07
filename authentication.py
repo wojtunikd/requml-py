5fb42dd965b8f8489f279dd839bd266f51746383
Damian <wojtunikdw@gmail.com>
2021-08-07 20:22:08 +0100

    Created JSON web token authentication for admins

---
+++ authentication.py
@@ -0,0 +1,27 @@
+import os
+import bcrypt
+import jwt
+import datetime
+
+from pymongo import MongoClient
+
+cluster = MongoClient(os.getenv("MONGODB_CONNECTION"))
+db = cluster["ReqUML"]
+admins = db["admins"]
+
+
+def getAdmin(email):
+    return admins.find_one({"email": email})
+
+
+def verifyPassword(adminPassword, passwordProvided):
+    return bcrypt.checkpw(passwordProvided.encode("utf-8"), adminPassword.encode("utf-8"))
+
+
+def generateJWT():
+    return jwt.encode({"exp": datetime.datetime.now() + datetime.timedelta(hours=3)}, os.getenv("JWT_SECRET"), algorithm="HS256")
+
+
+def decodeJWT(token):
+    return jwt.decode(token, os.getenv("JWT_SECRET"), algorithms=["HS256"])
+
