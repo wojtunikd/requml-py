c054bce6f05aecfde9518551616ddda8ada5fbff
Damian <wojtunikdw@gmail.com>
2021-08-10 21:25:55 +0100

    Reimplemented class analysis using spacy

--- Analysis/classes.py
+++ Analysis/classes.py
@@ -1,48 +1,85 @@
-from nltk import word_tokenize, pos_tag, RegexpParser
-from nltk.stem import WordNetLemmatizer
+import spacy
 
 
 def conductClassesAnalysis(sentences):
     potentialCandidates = identifyPotentialClassCandidates(sentences)
-    processedCandidates = processPotentialClassCandidates(potentialCandidates)
+    refinedCandidates = processPotentialClassCandidates(potentialCandidates)
+    print(refinedCandidates)
 
 
 def identifyPotentialClassCandidates(sentences):
+    nlp = spacy.load("en_core_web_sm")
     potentialClassCandidates = list()
 
     for sentence in sentences:
-        tokens = word_tokenize(sentence)
+        namedEntities = list()
 
-        tokens[0] = tokens[0].lower()
-        tokens.insert(0, "I")
+        doc = nlp(sentence)
 
-        taggedWords = pos_tag(tokens)
+        # Checking for named entities in a user story and preparing an exclusion list
+        for namedEntity in doc.ents:
+            namedEntities.append(*namedEntity.lemma_.split())
 
-        grammar = """NP: {<JJ>?<NN|NNS><NN|NNS>*}"""
-        results = RegexpParser(grammar).parse(taggedWords)
+        # Identifying noun chunks in each user story
+        for chunk in doc.noun_chunks:
+            candidate = {}
+            chunkWords = chunk.lemma_.split()
 
-        for subtree in results.subtrees():
-            if subtree.label() == "NP":
-                leafList = list()
-                for leaf in subtree.leaves():
-                    leafList.append(leaf[0])
-                potentialClassCandidates.append(leafList)
+            # Excluding any named entities from the list of words in the noun chunk to construct a general class name
+            wordsExcludingNE = [word for word in chunkWords if word not in namedEntities]
+
+            if len(wordsExcludingNE) < 1:
+                continue
+
+            headWordPOS = nlp(chunk.root.head.text)[0].pos_
+
+            # Using the dependency parsing within the user story, identifying potential methods/functions for each candidate class
+            if headWordPOS == "VERB":
+                rootChunk = "".join(list(map(lambda word: word.capitalize(), wordsExcludingNE)))
+                proposedFunction = chunk.root.head.lemma_.lower() + rootChunk + "()"
+                candidate["methods"] = [proposedFunction]
+
+            candidate["name"] = "_".join(list(map(lambda word: word.capitalize(), wordsExcludingNE)))
+
+            potentialClassCandidates.append(candidate)
 
     return potentialClassCandidates
 
 
+class ContinueForResolvedDuplicates(Exception):
+    pass
+
+
 def processPotentialClassCandidates(candidates):
-    processedCandidates = list()
-    lemmatizer = WordNetLemmatizer()
+    continueForResolved = ContinueForResolvedDuplicates()
+
+    refinedCandidates = list()
 
     for candidate in candidates:
-        lemmatizedCandidate = [lemmatizer.lemmatize(noun) for noun in candidate]
-        lemmatizedCandidate[0] = lemmatizedCandidate[0].capitalize()
+        listOfDuplicates = [element for element in candidates if element["name"] == candidate["name"]]
 
-        lemmatizedCandidate = " ".join(lemmatizedCandidate)
+        if len(listOfDuplicates) > 1:
+            try:
+                for existingCandidates in refinedCandidates:
+                    if existingCandidates["name"] == candidate["name"]:
+                        raise continueForResolved
+            except ContinueForResolvedDuplicates:
+                continue
 
-        processedCandidates.append(lemmatizedCandidate)
+            mergedCandidate = listOfDuplicates[0]
 
-    print(processedCandidates)
+            # Copying potential class methods to merge duplicates, if the method name is unique
+            for duplicate in listOfDuplicates:
+                if "methods" in duplicate:
+                    for method in duplicate["methods"]:
+                        if "methods" in mergedCandidate:
+                            if method not in mergedCandidate["methods"]:
+                                mergedCandidate["methods"] = [*mergedCandidate["methods"], method]
+                        else:
+                            mergedCandidate["methods"] = [method]
 
-    return processedCandidates
+            refinedCandidates.append(mergedCandidate)
+        else:
+            refinedCandidates.append(listOfDuplicates[0])
+
+    return refinedCandidates
