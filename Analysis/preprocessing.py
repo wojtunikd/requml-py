6b53a2c8878b32745b7b216e53244d6759f9a5c8
Damian <wojtunikdw@gmail.com>
2021-08-10 21:24:19 +0100

    Removed unnecessary stopwords processing

--- Analysis/preprocessing.py
+++ Analysis/preprocessing.py
@@ -1,11 +1,5 @@
 import string
-from nltk.corpus import stopwords
 
 
 def removePunctuation(sentence):
     return "".join([character for character in sentence if character not in string.punctuation])
-
-
-def removeStopwords(tokens):
-    englishStopwords = stopwords.words("english")
-    return [token for token in tokens if token not in englishStopwords]
