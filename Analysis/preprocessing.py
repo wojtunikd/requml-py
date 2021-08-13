c5a0a585ba3d227d2eba15892a35d7adc943ad8d
Damian <wojtunikdw@gmail.com>
2021-08-13 01:30:25 +0100

    Moved or created preprocessing rules

--- Analysis/preprocessing.py
+++ Analysis/preprocessing.py
@@ -3,3 +3,11 @@ import string
 
 def removePunctuation(sentence):
     return "".join([character for character in sentence if character not in string.punctuation])
+
+
+def getDomainSpecificWords():
+    return ["data"]
+
+
+def getPOSExclusion():
+    return ["JJR", "JJS", "PRP$", "DT", "RB"]
